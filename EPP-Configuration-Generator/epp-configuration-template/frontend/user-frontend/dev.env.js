b96e7ca581aa883b12f3d597dcdcd279e14d37a8
Featherb <23371015@buaa.edu.cn>
2026-04-19 04:43:35 +0800

    fix: 移除了配置相关文件的追踪

--- EPP-Configuration-Generator/epp-configuration-template/frontend/user-frontend/dev.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {{
-  NODE_ENV: '"development"',
-  VUE_APP_ROOT: '"http://127.0.0.1:8000"',
-  VUE_APP_API_ROOT: '"http://127.0.0.1:8000/api"'
-}}
--- EPP-Configuration-Generator/epp-configuration-template/frontend/user-frontend/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {{
-  NODE_ENV: '"production"',
-  VUE_APP_ROOT: '"http://{frontend|user|domain}:8080"',
-  VUE_APP_API_ROOT: '"http://{frontend|user|domain}:8080/api"'
-}}
